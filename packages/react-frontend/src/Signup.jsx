d4b6c97fb577bd4e974c6811db9bc2b6342fdf6a
Gbardasian <104814569+Gbardasian@users.noreply.github.com>
2024-05-26 12:51:17 -0700

    Create Signup.jsx
    Sign up page (No connection to backend yet)

---
+++ packages/react-frontend/src/Signup.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import styles from "./styles.css";
+import {
+  Center,
+  ChakraProvider,
+  Input,
+  Stack,
+  Image,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { object, string } from "yup";
+
+export default function LoginPage(): JSX.Element {
+  return (
+    <ChakraProvider resetCSS>
+      <Center h="100vh" className="border">
+        <Stack boxShadow="large" bg="white" p="20" rounded="md">
+          <Stack isInline>
+            <Image
+              src="https://us.123rf.com/450wm/kaissaart/kaissaart1807/kaissaart180700038/114801772-bee-flat-design-illustration-simple-vector-icon.jpg?ver=6"
+              maxW="90px"
+              mx="auto"
+            />
+          </Stack>
+          <Heading as="h1"> Sign Up </Heading>
+          <Text fontsize="lg" className="secText">
+            Please enter the information to register for Busy Bee Contacts.
+          </Text>
+
+          <Formik
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                console.log(values);
+                setSubmitting(false);
+              }, 1000);
+            }}
+            initialValues={{ email: "", password: "" }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Stack>
+                  <Input
+                    bg="lightgrey"
+                    name="name"
+                    type="name"
+                    placeholder="Full Name"
+                  />
+                  <Input
+                    bg="lightgrey"
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                  />
+                  <Input
+                    bg="lightgrey"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                  />
+
+                  <Button
+                    isLoasding={isSubmitting}
+                    loadingText="Whispering to our servers"
+                    className="but"
+                  >
+                    Sign Up
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+
+          <Stack justify="center" className="secText" spacing="3">
+            <Text align="center">
+              <span>Already have an account? </span>
+              <Button colorScheme="purple" variant="link">
+                Log In
+              </Button>
+            </Text>
+          </Stack>
+        </Stack>
+      </Center>
+    </ChakraProvider>
+  );
+}
