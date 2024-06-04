ebc68bfe0b2bda098846d7817e4376b474647563
Matteo T <89539137+matteoterrien@users.noreply.github.com>
2024-06-04 12:42:42 -0700

    deleting favorite contacts will automatically show instead of having tomanually rerender the screen

--- packages/express-backend/models/contact.js
+++ packages/express-backend/models/contact.js
@@ -14,22 +14,22 @@ const ContactSchema = new mongoose.Schema(
         phone_number: {
             type: String,
             trim: true,
-            validate(phone) {
-                var validRegex =
-                    /^[\+]?[0-9]{0,3}\W?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
-                if (!phone.match(validRegex))
-                    throw new Error('Invalid Phone Number. Try Again.')
-            },
+            // validate(phone) {
+            //     var validRegex =
+            //         /^[\+]?[0-9]{0,3}\W?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+            //     if (!phone.match(validRegex))
+            //         throw new Error('Invalid Phone Number. Try Again.')
+            // },
         },
         email: {
             type: String,
             trim: true,
-            validate(email) {
-                var validRegex =
-                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-                if (!email.match(validRegex))
-                    throw new Error('Invalid Email Address. Try Again.')
-            },
+            // validate(email) {
+            //     var validRegex =
+            //         /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+            //     if (!email.match(validRegex))
+            //         throw new Error('Invalid Email Address. Try Again.')
+            // },
         },
         address: {
             type: String,
